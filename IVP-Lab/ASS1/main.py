1a5090e0fd67790591b2bc9ddc77e039bea07519
ARIF MULANI <amnetwork049@gmail.com>
2024-01-20 10:26:26 +0530

    rgb to gray

--- IVP-Lab/ASS1/main.py
+++ IVP-Lab/ASS1/main.py
@@ -1,2 +1,18 @@
 import cv2 as cv
 
+# Read the original image
+img = cv.imread('input_imgs/parrot.jpg')
+
+# Convert the image to grayscale
+grayScale_img = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
+
+# Display the original and grayscale images
+cv.imshow("parrot", img)
+cv.imshow("grayscale parrot", grayScale_img)
+
+# Wait for a key event and then close the windows
+cv.waitKey(0)
+cv.destroyAllWindows()
+
+# Save the grayscale image to the output_imgs folder
+cv.imwrite('output_imgs/gray_parrot.jpg', grayScale_img)
