af91821f4204aa0d61ca49f5799f82049d5f593f
ARIF MULANI <amnetwork049@gmail.com>
2024-01-20 10:34:56 +0530

    negative code added

--- IVP-Lab/ASS1/main.py
+++ IVP-Lab/ASS1/main.py
@@ -7,12 +7,26 @@ img = cv.imread('input_imgs/parrot.jpg')
 grayScale_img = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
 
 # Display the original and grayscale images
-cv.imshow("parrot", img)
-cv.imshow("grayscale parrot", grayScale_img)
+cv.imshow("Original Parrot", img)
+cv.imshow("Grayscale Parrot", grayScale_img)
 
 # Wait for a key event and then close the windows
 cv.waitKey(0)
-cv.destroyAllWindows()
+cv.destroyWindow("Original Parrot")
+cv.destroyWindow("Grayscale Parrot")
 
-# Save the grayscale image to the output_imgs folder
+# Create a negative image by subtracting pixel values from 255
+neg_img = 255 - img
+
+# Display the original and negative images
+cv.imshow("Original Parrot", img)
+cv.imshow("Negative Parrot", neg_img)
+
+# Wait for a key event and then close the windows
+cv.waitKey(0)
+cv.destroyWindow("Original Parrot")
+cv.destroyWindow("Negative Parrot")
+
+# Save the grayscale and negative images to the output_imgs folder
 cv.imwrite('output_imgs/gray_parrot.jpg', grayScale_img)
+cv.imwrite('output_imgs/negative_parrot.jpg', neg_img)
